aaaa70881b1499eed51b9038a8ba41d35d9002ca
James <bakerje@mac.com>
2017-09-26 13:17:23 +0100

    Ex 19

--- ex19.py
+++ ex19.py
@@ -1,2 +1,20 @@
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print(f"You have {cheese_count} cheeses!")
+    print(f"You have {boxes_of_crackers} boxes of crackers!")
+    print("That's enough for a party!")
+    print("Get a blanket.\n")
+
+
+print("We can just the function numbers directly")
+cheese_and_crackers(20, 30)
+
+print("Or, we can use variables from out script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+cheese_and_crackers(10 + 20, 20 + 50)
+
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
