806c0c76a697a6e9c5e7dc97f90466528cef152a
James <bakerje@mac.com>
2017-10-03 16:25:41 +0100

    Added shell

---
+++ shell.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# sysinfo_page - A script to produce an HTML file
+
+cat <<-_EOF_
+<html>
+<head>
+	<title>
+	My system information.
+	</title>
+</head>
+
+<body>
+<h1>My system information</h1>
+</body>
+</html>
+_EOF_
