e4b5131069a0da9b8153de549ab9e58f60ba2e05
James <bakerje@mac.com>
2017-10-03 15:48:09 +0100

    Ex 33 with method

--- ex33.py
+++ ex33.py
@@ -1,13 +1,21 @@
 i = 0
 numbers = []
+limit  = 6
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
+#while i < 6:
+#    print(f"At the top i is {i}")
+#    numbers.append(i)
 
-    i += 1
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}")
+def while_loop(i):
+    if i < limit:
+        print(f"At the top i is {i}")
+        numbers.append(i)
+        i += 1
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
+        while_loop(i)
+
+while_loop(i)
 
 print("The numbers: ")
 
