90967b5dd2f9ea117479d074a122650450c2db93
James <bakerje@mac.com>
2017-10-07 13:18:07 +0100

    Ex 38 1

--- ex38_1.py
+++ ex38_1.py
@@ -32,5 +32,22 @@ print('-' * 10)
 for abbrev, city in list(cities.items()):
     print(f"{abbrev} has the city {city}")
 
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} state is abbreviated to {abbrev}")
+    print(f"and has city {cities[abbrev]}")
+
+print('-' * 10)
+state = states.get('Texas')
+
+if not state:
+    print("Sorry, no Texas")
+
+city = cities.get('FL', 'Does Not Exist')
+print(f"The city for the state 'TX' is {city}")
+
+
+
+
     
 
