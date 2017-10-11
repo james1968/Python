7f91792c5336c7c344c7641019d1300396310b6c
James <bakerje@mac.com>
2017-10-11 17:26:53 +0100

    SHell

--- shell.sh
+++ shell.sh
@@ -2,19 +2,21 @@
 
 # sysinfo_page - A script to produce an HTML file
 
-title="My System Infotmation"
+title="My System Infotmation for $HOSTNAME"
+RIGHT_NOW=$(date +"%x %r %Z")
+TIME_STAMP="Updated on $RIGHT_NOW by $USER"
 
 cat <<-_EOF_
 <html>
 <head>
 	<title>
-	$title $HOSTNAME
+	$title
 	</title>
 </head>
 
 <body>
-<h1>$title $HOSTNAME</h1>
-<p>Updated on $(date +"%x %r %Z") by $USER</p>
+<h1>$title</h1>
+<p>$TIME_STAMP</p>
 </body>
 </html>
 _EOF_
